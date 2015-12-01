3f9a596face2acb7908275b71ffbc35ea1ce7659
Stephane Landelle <slandelle@gatling.io>
2015-12-01 12:28:58 +0100

    Don't leak connection on websocket redirect

--- client/src/main/java/org/asynchttpclient/netty/handler/WebSocketProtocol.java
+++ client/src/main/java/org/asynchttpclient/netty/handler/WebSocketProtocol.java
@@ -19,6 +19,7 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpHeaderValues;
+import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponse;
 import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.CloseWebSocketFrame;
@@ -69,29 +70,22 @@ public final class WebSocketProtocol extends Protocol {
 
         private final Channel channel;
         private final HttpResponse response;
+        private final  WebSocketUpgradeHandler handler;
+        private final  HttpResponseStatus status;
+        private final  HttpResponseHeaders responseHeaders;
         
-        public UpgradeCallback(NettyResponseFuture<?> future, Channel channel, HttpResponse response) {
+        public UpgradeCallback(NettyResponseFuture<?> future, Channel channel, HttpResponse response, WebSocketUpgradeHandler handler, HttpResponseStatus status, HttpResponseHeaders responseHeaders) {
             super(future);
             this.channel = channel;
             this.response = response;
+            this.handler = handler;
+            this.status = status;
+            this.responseHeaders = responseHeaders;
         }
         
         @Override
         public void call() throws Exception {
             
-            WebSocketUpgradeHandler handler = WebSocketUpgradeHandler.class.cast(future.getAsyncHandler());
-            Request request = future.getCurrentRequest();
-            
-            HttpResponseStatus status = new NettyResponseStatus(future.getUri(), config, response, channel);
-            HttpResponseHeaders responseHeaders = new HttpResponseHeaders(response.headers());
-            Realm realm = request.getRealm() != null ? request.getRealm() : config.getRealm();
-
-            if (exitAfterProcessingFilters(channel, future, handler, status, responseHeaders)) {
-                return;
-            }
-
-            if (REDIRECT_STATUSES.contains(status.getStatusCode()) && exitAfterHandlingRedirect(channel, future, response, request, response.status().code(), realm))
-                return;
 
             boolean validStatus = response.status().equals(SWITCHING_PROTOCOLS);
             boolean validUpgrade = response.headers().get(HttpHeaderNames.UPGRADE) != null;
@@ -137,7 +131,26 @@ public final class WebSocketProtocol extends Protocol {
 
         if (e instanceof HttpResponse) {
             HttpResponse response = (HttpResponse) e;
-            Channels.setAttribute(channel, new UpgradeCallback(future, channel, response));
+            if (logger.isDebugEnabled()) {
+                HttpRequest httpRequest = future.getNettyRequest().getHttpRequest();
+                logger.debug("\n\nRequest {}\n\nResponse {}\n", httpRequest, response);
+            }
+            
+            WebSocketUpgradeHandler handler = WebSocketUpgradeHandler.class.cast(future.getAsyncHandler());
+            HttpResponseStatus status = new NettyResponseStatus(future.getUri(), config, response, channel);
+            HttpResponseHeaders responseHeaders = new HttpResponseHeaders(response.headers());
+            
+            Request request = future.getCurrentRequest();
+            Realm realm = request.getRealm() != null ? request.getRealm() : config.getRealm();
+
+            if (exitAfterProcessingFilters(channel, future, handler, status, responseHeaders)) {
+                return;
+            }
+
+            if (REDIRECT_STATUSES.contains(status.getStatusCode()) && exitAfterHandlingRedirect(channel, future, response, request, response.status().code(), realm))
+                return;
+            
+            Channels.setAttribute(channel, new UpgradeCallback(future, channel, response, handler, status, responseHeaders));
 
         } else if (e instanceof WebSocketFrame) {
 
